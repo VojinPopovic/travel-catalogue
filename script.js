a67d2689e3b869969c9c8823b39b99601280234f
VojinPopovic <vojinpopovic3@gmail.com>
2022-08-03 18:34:20 +0200

    fixed slider bug

--- script.js
+++ script.js
@@ -1,41 +1,42 @@
 class SliderPopular {
   constructor(configPopular) {
     this.popularCountry = configPopular;
+    this.imgNum = 0;
     this.sliderLeft();
-    this.sliderRight()
+    this.sliderRight();
   }
   sliderLeft() {
     let image = this.popularCountry.images;
     let imageDiv = document.querySelector(".picture-slider-container");
     let btnLeft = document.querySelector(".btn-left");
-    let imgNum = 0;
     btnLeft.addEventListener("click", () => {
       this.removeSrc(imageDiv);
-      if (imgNum < 0) {
-        imgNum = image.length - 1;
-        imageDiv.style.backgroundImage = `url("${image[imgNum]}")`;
-        imageDiv.style.transition = "all 0.5s";
+
+      this.imgNum--;
+      if (this.imgNum < 0) {
+        this.imgNum = image.length - 1;
+        imageDiv.style.backgroundImage = `url("${image[this.imgNum]}")`;
       } else {
-        imageDiv.style.backgroundImage = `url("${image[imgNum]}")`;
-        imageDiv.style.transition = "all 0.5s";
+        imageDiv.style.backgroundImage = `url("${image[this.imgNum]}")`;
       }
-      imgNum--;
+      console.log(this.imgNum);
     });
   }
   sliderRight() {
     let image = this.popularCountry.images;
     let imageDiv = document.querySelector(".picture-slider-container");
     let btnRight = document.querySelector(".btn-right");
-    let imgNum = 0;
     btnRight.addEventListener("click", () => {
       this.removeSrc(imageDiv);
-      if ((imgNum >= image.length)) {
-        imgNum = 0;
-        imageDiv.style.backgroundImage = `url("${image[imgNum]}")`;
+      this.imgNum++;
+      if (this.imgNum >= image.length) {
+        this.imgNum = 0;
+        imageDiv.style.backgroundImage = `url("${image[this.imgNum]}")`;
       } else {
-        imageDiv.style.backgroundImage = `url("${image[imgNum]}")`;
+        imageDiv.style.backgroundImage = `url("${image[this.imgNum]}")`;
       }
-      imgNum++;
+
+      console.log(this.imgNum);
     });
   }
   removeSrc(imageDiv) {

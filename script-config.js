0c355f4067aca49744381c9e5c5ceafd4398f63f
VojinPopovic <vojinpopovic3@gmail.com>
2022-08-03 17:49:34 +0200

    main image slider added

---
+++ script-config.js
@@ -0,0 +1,17 @@
+config = {
+  iceland: {
+    capital: "Reykjavík",
+    native_language: "Icelandic",
+    images: ["/images/iceland", "/images/iceland1", "/images/iceland2"],
+  },
+  sweden: {},
+};
+
+configPopular = {
+  country: "iceland",
+  capital: "Reykjavík",
+  native_language: "Icelandic",
+  images: ["/images/iceland.jpg", "/images/iceland1.jpg", "/images/iceland2.jpg"],
+};
+
+let sliderPopular = new SliderPopular(configPopular);
--- script.js
+++ script.js
@@ -1,10 +1,44 @@
-class Object {
-  constructor() {
-    this.verify = 5;
-    this.verifyImePrezime();
+class SliderPopular {
+  constructor(configPopular) {
+    this.popularCountry = configPopular;
+    this.sliderLeft();
+    this.sliderRight()
   }
-
-  verifyImePrezime() {}
+  sliderLeft() {
+    let image = this.popularCountry.images;
+    let imageDiv = document.querySelector(".picture-slider-container");
+    let btnLeft = document.querySelector(".btn-left");
+    let imgNum = 0;
+    btnLeft.addEventListener("click", () => {
+      this.removeSrc(imageDiv);
+      if (imgNum < 0) {
+        imgNum = image.length - 1;
+        imageDiv.style.backgroundImage = `url("${image[imgNum]}")`;
+        imageDiv.style.transition = "all 0.5s";
+      } else {
+        imageDiv.style.backgroundImage = `url("${image[imgNum]}")`;
+        imageDiv.style.transition = "all 0.5s";
+      }
+      imgNum--;
+    });
+  }
+  sliderRight() {
+    let image = this.popularCountry.images;
+    let imageDiv = document.querySelector(".picture-slider-container");
+    let btnRight = document.querySelector(".btn-right");
+    let imgNum = 0;
+    btnRight.addEventListener("click", () => {
+      this.removeSrc(imageDiv);
+      if ((imgNum >= image.length)) {
+        imgNum = 0;
+        imageDiv.style.backgroundImage = `url("${image[imgNum]}")`;
+      } else {
+        imageDiv.style.backgroundImage = `url("${image[imgNum]}")`;
+      }
+      imgNum++;
+    });
+  }
+  removeSrc(imageDiv) {
+    imageDiv.style.backgroundImage = "none";
+  }
 }
-
-let ooo = new Object();
